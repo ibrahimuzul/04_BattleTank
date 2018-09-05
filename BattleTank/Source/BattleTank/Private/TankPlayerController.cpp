2f2c4f909795f3c55169c3d06efd21b7907c996f
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-05 14:33:38 +0300

    136 Using LineTraceSingleByChannel()

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -41,7 +41,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; //Out parameter
 	if (GetSightRayHitLocation(HitLocation)) // Has side-effect , is going to line trace
 	{
-		// UE_LOG(LogTemp, Warning, TEXT("Look direction : %s"), *HitLocation.ToString());
+		 UE_LOG(LogTemp, Warning, TEXT("Hit location : %s"), *HitLocation.ToString());
 
 		// TODO Tell controlled tank to aim at this point
 	}
@@ -50,7 +50,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 // Get world location  of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {
-	HitLocation = FVector(1.0);
+	
 
 	//Find the crosshair position in pixel coordinates
 	int32 ViewportSizeX, ViewportSizeY;
@@ -62,21 +62,46 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 	FVector LookDirection;
 	if (GetLookDirection(ScreenLocation, LookDirection))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Look direction : %s"), *LookDirection.ToString());
-	}
-	
 
-	// Line-trace along that LookDirection, and see what we hit (up to max range)
+		// Line-trace along that LookDirection, and see what we hit (up to max range)
+		// GetLookVectorHitLocation();
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction : %s"), *LookDirection.ToString());
+	}
+
+
 	return true;
 }
 
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility))// Line trace succeeds
+	{
+		// Set hit location
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	HitLocation = FVector(0, 0, 0);
+	return false;
+}
+
+
 bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
 {
-	
+
 	FVector CameraWorldLocation; // To be discarded
 
 	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
 
-	
+
 
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -33,7 +33,10 @@ private:
 		float CrosshairXLocation = 0.5;
 	UPROPERTY(EditAnywhere)
 		float CrosshairYLocation = 0.33333;
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000;//10 km
 
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 };
