1d5d516b4bad2652f89dbd61e8974a9982f27bf0
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-06 17:45:25 +0300

    150 CHALLENGE - Turret Rotation pt.2

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -23,7 +23,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true; // TODO Should this really tick ?
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -111,5 +111,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	UE_LOG(LogTemp, Warning, TEXT("DeltaRotator: %s"), *DeltaRotator.ToString());
 
 	Barrel->Elevate(DeltaRotator.Pitch);
-
+	Turret->Rotate(DeltaRotator.Yaw);
 }
--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -3,5 +3,16 @@
 #include "TankTurret.h"
 
 
+void UTankTurret::Rotate(float RelativeSpeed)
+{
+	
 
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+
+	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange;// 
+	//auto Rotation = FMath::Clamp<float>(RawNewRotation, MinRotationDegrees, MaxRotationDegrees);
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+
+}
 
