3d812a5363bcc9de9f24feeb7f696e1ce66ca25e
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-07 20:48:41 +0300

    156 Projectile Movement Components

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -50,17 +50,17 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-
-	UE_LOG(LogTemp, Warning, TEXT("%f : Tank fires"), Time);
+	
 
 	if (!Barrel) { return; }
 
 	// Spawn a projectile
 
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 		);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Runtime/Engine/Classes/GameFramework/ProjectileMovementComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Projectile.generated.h"
@@ -15,14 +16,16 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void LaunchProjectile(float Speed);
+
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
 	
 };
