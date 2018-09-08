a7f3e2fc6eb439b2ddadcfcc8cb30d73cb84fa07
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-08 14:03:53 +0300

    161 ApplyForceAtLocation() in Action

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -13,11 +13,13 @@ UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+
 public:
 	// Sets a throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
-	void SetThrottle(float Throttle);
-	
+		void SetThrottle(float Throttle);
 
+	// Max force per track , in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank and 1 g acceleration
 };
