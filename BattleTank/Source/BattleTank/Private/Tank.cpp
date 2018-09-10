dc019cafb9af82972bc0437e3ab690e85001fca8
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-10 21:12:01 +0300

    178 Decoupling Your Architecture

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -37,7 +37,8 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay(); // Needed for BP begin Play to run !
-	
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank C++ Begin Play"), *TankName);
 }
 
 
