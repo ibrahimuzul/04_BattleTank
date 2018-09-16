a09611811ecbd6d798b79f5291f1e3efa1c600ef
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-16 20:29:51 +0300

    188 Programmatic Sideways Friction - 2

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,7 +4,24 @@
 #include "BattleTank.h"
 
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
 
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	//Super::TickComponent(DeltaTime, TickType,*ThisTickFunction);
+	//UE_LOG(LogTemp, Warning, TEXT("I Am  Here"));
+	// cALculate the slippage speed
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	// Work out the required acceleration this frame to correct
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	// calculate and apply sideways for (F=ma)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration / 2;// Two Tracks
+	TankRoot->AddForce(CorrectionForce);
+}
 
 
 void UTankTrack::SetThrottle(float Throttle)
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -22,4 +22,10 @@ public:
 	// Max force per track , in Newtons
 	UPROPERTY(EditDefaultsOnly)
 		float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank and 1 g acceleration
+
+private:
+	UTankTrack();
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 };
