144d74c90fb13c7ccd871fe55e10558a15a91edf
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-16 19:19:45 +0300

    189 OnComponentHit Event in 4.12 (4.21)

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,6 +9,18 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnHit"));
+}
+
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	//Super::TickComponent();
@@ -18,7 +30,7 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 	// Work out the required acceleration this frame to correct
 	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
 	// calculate and apply sideways for (F=ma)
-	auto TankRoot =Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration / 2;// Two Tracks
 	TankRoot->AddForce(CorrectionForce);
 }
